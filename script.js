4205d0613e9ff183b9a277c85a7902c2da3ae47e
João Pedro Paranhos <joaopedroparanhosdefarias@gmail.com>
2025-03-17 17:32:16 -0300

    script/atualizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 location = "https://api.whatsapp.com/send/?phone=5541999446102&text=Olá%2C+gostaria+de+saber+mais+como+eu+posso+alavancar+meu+patrimônio+com+consorcio&type=phone_number&app_absent=0"
-document.getElementById("meuBotao").addEventListener("click", function() {
+document.getElementById("btn_ctt").addEventListener("click", function() {
     window.location.href = location;
     // ou para abrir em nova aba:
     // window.open("https://www.exemplo.com", "_blank");
