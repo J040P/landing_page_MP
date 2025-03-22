b25bc0d7f1c806518d6ce5cec1aaac1c1537c486
João Pedro Paranhos <joaopedroparanhosdefarias@gmail.com>
2025-03-21 22:14:17 -0300

    script3

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-document.getElementById("btn_ctt").addEventListener("click", function() {
-    window.open("https://api.whatsapp.com/send/?phone=5541999446102&text=Olá%2C+gostaria+de+saber+mais+como+eu+posso+alavancar+meu+patrimônio+com+consorcio&type=phone_number&app_absent=0","_blank");
-    // ou para abrir em nova aba:
-    // window.open("https://www.exemplo.com", "_blank");
+document.querySelectorAll("#btn_ctt").forEach(button => {
+    button.addEventListener("click", function() {
+        window.open("https://api.whatsapp.com/send/?phone=5541999446102&text=Olá%2C+gostaria+de+saber+mais+como+eu+posso+alavancar+meu+patrimônio+com+consorcio&type=phone_number&app_absent=0", "_blank");
+    });
 });
 
-
-
-document.getElementById("btn_simula").addEventListener("click", function() {
-    window.open("https://autorizado.ademicon.com.br/marcia-paranhos");
-    // ou para abrir em nova aba:
-    // window.open("https://www.exemplo.com", "_blank");
+document.querySelectorAll("#btn_simula").forEach(button => {
+    button.addEventListener("click", function() {
+        window.location.href = "https://autorizado.ademicon.com.br/marcia-paranhos";
+    });
 });
